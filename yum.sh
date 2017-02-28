7619fd2d3988809527ed1b738c93dc290e5bde88
Vamshi <kvamshikanth@gmail.com>
2017-02-28 09:21:51 -0800

    fixed yum.sh

--- yum.sh
+++ yum.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear;
 echo -e "server :" `cat /root/.rackspace/server_number` "\n"
 echo "OS :" `cat /etc/redhat-release`
 
@@ -25,7 +26,7 @@ if [[ $last_log =~ \.gz$ ]];
 		echo -en "$last_log\nLast Modified :"; 
 		stat -c %y $last_log; 
 		echo -n "Packages last updated: "; 
-		cat $last_log | awk '{print $1,2}' | uniq | tail -1; 	
+		cat $last_log | awk '{print $1,$2}' | uniq | tail -1; 	
 fi;
 
 echo -e "\nLast yum update run :"
@@ -33,26 +34,46 @@ grep -i yum /var/log/cron | awk '{print $1,$2,$6,$7}' | uniq | tail -2
 
 echo -e "\nKernel updates :"
 echo "================="
-last_kernel_update=$(rpm -qa | grep -i ^kernel-[0-9]| sort -rn | grep el[0-9] | tail -1 | cut -d- -f2,3,4)
-kernel_loaded=$(uname -r)
 
-if [[ $last_kernel_update == $kernel_loaded ]]; 
-	then 
+## Checking Kernel installed, lastest kernel installed and latest_kernel_available.
+latest_kernel_installed=$(rpm -qa | grep -i ^kernel-[0-9]| sort -rn | grep el[0-9] | head -1 | cut -d- -f2,3,4)
+#echo "latest_kernel_installed = $latest_kernel_installed"
+
+kernel_loaded=$(uname -r)
+#echo "kernel_loaded = $kernel_loaded"
+
+latest_kernel_available=$(yum check-update kernel --disableexcludes=all 2>&1 | grep ^kernel  | awk '{print $2}')
+#echo "latest_kernel_available = $latest_kernel_available"
+
+## is the loaded kernel the latest installed one ??
+
+if [[ $latest_kernel_installed == $kernel_loaded ]]; then 
 		echo -n "Server is on the most recent kernel installed : "; 
+		loaded_latest_kernel_installed="yes"
 		echo $kernel_loaded; 
 	else 
-		echo "Last kernel update is : $last_kernel_update" ; 
-		echo "Server is on the kernel $kernel_loaded"; 
-		echo "This server will need a reboot"; 
+		echo "Lastest kernel installed is : $latest_kernel_installed" ; 
+		echo "BUT Server is on the kernel $kernel_loaded"; 
+		loaded_latest_kernel_installed="no"
 fi
 
+## 
 
-new_kernel=$(yum check-update kernel --disableexcludes=all 2>&1 | grep ^kernel  | awk '{print $2}')
-if [[ $last_kernel_update == $new_kernel ]]; 
-	then 
-		echo -e "\nServer is up-to-date"; 
-	else 
-		echo -e "\nHowever, Kernel will need update.\nLastest kenrel available is : $new_kernel" ; 
-		echo "Server is on the kernel: $kernel_loaded"; 
+
+#if [[ $latest_kernel_installed == $latest_kernel_available ]] && [[ $loaded_latest_kernel_installed == "yes" ]]; then 
+if [[ -z $latest_kernel_available ]]  && [[ $loaded_latest_kernel_installed == "yes" ]]; then
+	echo -e "\nServer is up-to-date"
+	echo -e "And is Loaded into the Latest Kernel"
+
+elif [[ -z $latest_kernel_available ]] && [[ $loaded_latest_kernel_installed == "no" ]]; then
+	echo -e "Server has latest Kernel"
+	echo -e "But needs reboot to boot into the new kernel $latest_kernel_installed" 
+#	echo 	"Server will need a kernel update and reboot
+#		A reboot is needed anyways even if you choose not to update
+#		... As the server is not loaded from the latest kernel installed."
+
+else 
+	echo -e "\nHowever, Kernel will need update.\nLastest kenrel available is : $new_kernel" ; 
+	echo "Server is on the kernel: $kernel_loaded"; 
 fi;
 
